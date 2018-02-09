03c4472419129cbd275c651cc6ba078e3110c7f9
Gian Felipe <gianfelipe93@gmail.com>
2018-02-10 09:26:57 +1100

    Start using action creators and replicating the action returned between the reducers

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
+import { selectBook } from '../actions/index'
+import { bindActionCreators } from 'redux'
+
 class BookList extends Component {
     renderList(){
         return this.props.books.map(book => {
@@ -20,9 +23,22 @@ class BookList extends Component {
 }
 
 function mapStateToProps(state) {
+    //Whenever is return will show up as props
+    //on the Booklist Container
     return {
         books: state.books
     }
 }
 
-export default connect(mapStateToProps)(BookList)
+//Anything returned from this function will end up as props
+//on the BookList container.
+function mapDispatchToProps(dispatch) {
+    //Whenever selectBook is called, the result should be passed
+    //to all of our reducers
+    return bindActionCreators({selectBook: selectBook}, dispatch)
+}
+
+// Promote BookList from a component to a container
+//it needs to know about this new dispatch method, selectBook,
+// Make it available as a prop
+export default connect(mapStateToProps, mapDispatchToProps)(BookList)
